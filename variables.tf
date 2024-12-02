ec5cfe5c622e9b55a2142a99b23bbf0bf3994618
Jennifer Moore <Jennifer.Moore@nelnetservicing.net>
2024-12-02 10:13:56 -0600

    fix

--- variables.tf
+++ variables.tf
@@ -10,12 +10,14 @@ variable "region" {
 
 variable "prefix" {
   type        = string
-  description = "(Required) Prefix to use for all resources in this module. Default: globo-dev"
+  description = "(Optional) Prefix to use for all resources in this module. Default: globo-dev"
+  default     = "globo-dev"
 }
 
 variable "environment" {
   type        = string
-  description = "(Required) Environment of all RESOURCES"
+  description = "(Optional) Environment of all RESOURCES"
+  default     = "development"
 }
 
 variable "billing_code" {
@@ -25,11 +27,16 @@ variable "billing_code" {
 
 variable "cidr_block" {
   type        = string
-  description = "(Required) The CIDR block for the VPC. Default:10.42.0.0/16"
+  description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
+  default     = "10.42.0.0/16"
 }
 
 variable "public_subnets" {
   type        = map(string)
-  description = "(Required) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
+  description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: {subnet-1 ="
+  default = {
+    public-1 = "10.42.10.0/24"
+    public-2 = "10.42.11.0/24"
+  }
 }
 
